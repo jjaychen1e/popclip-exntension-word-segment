fb03eccc1a5be92ce6f3f2fb773c3990f2b9ff72
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2022-02-24 23:06:16 +0800

    fix: Retain `:` in the input text.

--- Word Segment.popclipext/word_segment.py
+++ Word Segment.popclipext/word_segment.py
@@ -4,10 +4,10 @@ import sys
 
 words = sys.argv[1]
 load()
-words = re.split('(\.|\!|\?|\,)', words)
+words = re.split('(\.|\!|\?|\,|\:)', words)
 res = []
 for w in words:
-    if '.' in w or '!' in w or '?' in w or ',' in w:
+    if '.' in w or '!' in w or '?' in w or ',' in w or ':' in w:
         res[-1] += w
     else:
         res.append(" ".join(segment(w)))
